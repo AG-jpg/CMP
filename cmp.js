ef98ba6436d0b05c8d96ccaeff22d443cebedadc
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2021-04-09 18:55:19 +0300

    CMP w/Sticky Notes
    I added sticky notes and change some UI.
    Added JQuery UI.

--- cmp.js
+++ cmp.js
@@ -7,7 +7,7 @@ let button=document.getElementById("button");
 button.addEventListener("change", function(){
     console.log(this.files[0].name)
     console.log(this.files[0].type)
-    if(this.files[0].type != 'image/jpeg'&&this.files[0].type != 'image/png'){
+    if(this.files[0].type != 'image/jpeg' && this.files[0].type != 'image/png'){
         alert("You can only uplaod JPG or PNG Files");
     }else{
         img.src=URL.createObjectURL(this.files[0])
@@ -16,7 +16,15 @@ button.addEventListener("change", function(){
 
 //Tools
 $(document).ready(function(){
-    //Draggable
-    console.log("Listo!")
+
+    //Draggable Item
     $('.item').draggable();
+
+    //Draggable-Resizable Note
+    $('.note').draggable();
+    $('.note').resizable();
+    
+    $('#add_note').on('click', function(){
+        console.log("Hello!");
+    });
 });
